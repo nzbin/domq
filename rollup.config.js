e283dda1117a438f86a44cdb7cdf9eb592bd4b6c
nzbin <nzb329@163.com>
2018-12-09 11:12:15 +0800

    add new files

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+const banner = `
+/*!
+ * ${pkg.name} - v${pkg.version}
+ * ${pkg.description}
+ * ${pkg.homepage}
+ *
+ * Copyright (c) 2018 ${pkg.author}
+ * Released under ${pkg.license} License
+ */
+`;
+
+
+export default [
+  {
+    input: 'src/js/core.js',
+    output: [
+      {
+        name: 'd',
+        banner,
+        file: "dist/d.js",
+        format: 'umd',
+        sourcemap: true
+      },
+      {
+        banner,
+        file: 'dist/d.common.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        banner,
+        file: 'dist/d.esm.js',
+        format: 'es',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      babel({ exclude: 'node_modules/**' }),
+      resolve(),
+      commonjs(),
+    ],
+  },
+];
