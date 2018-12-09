1b4faab8f82841fa643c3d816697987892022b05
nzbin <nzb329@163.com>
2018-12-09 11:04:21 +0800

    rebuild code structure

--- src/core.js
+++ src/core.js
@@ -7,8 +7,7 @@
 ;
 (function () {
 
-    var undefined,
-        key,
+    var key,
         classList,
         emptyArray = [],
         concat = emptyArray.concat,
@@ -69,8 +68,9 @@
         isArray = Array.isArray || function (object) { return object instanceof Array };
 
     function type(obj) {
-        return obj == null ? String(obj) :
-            class2type[toString.call(obj)] || "object"
+        return obj == null
+            ? String(obj)
+            : class2type[toString.call(obj)] || "object"
     }
 
     function isFunction(value) {
@@ -125,18 +125,81 @@
 
     // Class D
     var D = function (selector, context) {
-        return D.fn.init(selector, context)
+        return new D.fn.init(selector, context)
     }
 
-    // Make Array
-    D.init = function (dom, selector) {
-        var i, len = dom ? dom.length : 0
-        for (i = 0; i < len; i++) this[i] = dom[i]
-        this.length = len
-        this.selector = selector || ''
+    D.fn = D.prototype = {
+        constuctor: D,
+        length: 0,
+        // Because a collection acts like an array
+        // copy over these useful array functions.
+        forEach: emptyArray.forEach,
+        reduce: emptyArray.reduce,
+        push: emptyArray.push,
+        sort: emptyArray.sort,
+        splice: emptyArray.splice,
+        indexOf: emptyArray.indexOf,
+        // D's counterpart to jQuery's `$.fn.init` and
+        // takes a CSS selector and an optional context (and handles various
+        // special cases).
+        // This method can be overridden in plugins.
+        init: function (selector, context) {
+            var dom;
+            // If nothing given, return an empty D collection
+            if (!selector) {
+                return this
+            }
+            // Optimize for string selectors
+            else if (typeof selector == 'string') {
+                selector = selector.trim()
+                // If it's a html fragment, create nodes from it
+                // Note: In both Chrome 21 and Firefox 15, DOM error 12
+                // is thrown if the fragment doesn't begin with <
+                if (selector[0] == '<' && fragmentRE.test(selector)) {
+                    dom = D.fragment(selector, RegExp.$1, context);
+                    selector = null;
+                }
+                // If there's a context, create a collection on that context first, and select
+                // nodes from there
+                else if (context !== undefined) {
+                    return D(context).find(selector)
+                }
+                // If it's a CSS selector, use it to select nodes.
+                else {
+                    dom = D.qsa(document, selector)
+                }
+            }
+            // If a function is given, call it when the DOM is ready
+            else if (isFunction(selector)) {
+                return D(document).ready(selector)
+            }
+            // If a D collection is given, just return it
+            else if (D.isD(selector)) {
+                return selector
+            }
+            // normalize array if an array of nodes is given
+            else if (isArray(selector)) {
+                dom = compact(selector)
+            }
+            // Wrap DOM nodes.
+            else if (isObject(selector)) {
+                dom = [selector], selector = null
+            }
+            // If there's a context, create a collection on that context first, and select
+            // nodes from there
+            else if (context !== undefined) {
+                return D(context).find(selector)
+            }
+            // And last but no least, if it's a CSS selector, use it to select nodes.
+            else {
+                dom = D.qsa(document, selector)
+            }
+            // create a new D collection from the nodes found
+            return D.makeArray(dom, selector, this);
+        }
     }
 
-    D.extend = function () {
+    D.extend = D.fn.extend = function () {
         var options, name, src, copy, copyIsArray, clone,
             target = arguments[0] || {},
             i = 1,
@@ -191,131 +254,130 @@
         }
         // Return the modified object
         return target;
-    };
-
-    // The function takes a html string and an optional tag name
-    // to generate DOM nodes from the given html string.
-    // The generated DOM nodes are returned as an array.
-    // This function can be overridden in plugins for example to make
-    // it compatible with browsers that don't support the DOM fully.
-    D.fragment = function (html, name, properties) {
-        var dom, nodes, container
-
-        // A special case optimization for a single tag
-        if (singleTagRE.test(html)) {
-            dom = D(document.createElement(RegExp.$1))
-        }
-
-        if (!dom) {
-
-            if (html.replace) {
-                html = html.replace(tagExpanderRE, "<$1></$2>")
-            }
-
-            if (name === undefined) {
-                name = fragmentRE.test(html) && RegExp.$1
-            }
-
-            if (!(name in containers)) {
-                name = '*'
-            }
-
-            container = containers[name]
-            container.innerHTML = '' + html
-            dom = D.each(slice.call(container.childNodes), function () {
-                container.removeChild(this)
-            })
-        }
-
-        if (isPlainObject(properties)) {
-            nodes = D(dom)
-            D.each(properties, function (key, value) {
-                if (methodAttributes.indexOf(key) > -1) nodes[key](value)
-                else nodes.attr(key, value)
-            })
-        }
-
-        return dom
     }
 
-    // D's CSS selector implementation which
-    // uses `document.querySelectorAll` and optimizes for some special cases, like `#id`.
-    // This method can be overridden in plugins.
-    D.qsa = function (element, selector) {
-        var found,
-            maybeID = selector[0] == '#',
-            maybeClass = !maybeID && selector[0] == '.',
-            // Ensure that a 1 char tag name still gets checked
-            nameOnly = maybeID || maybeClass ? selector.slice(1) : selector,
-            isSimple = simpleSelectorRE.test(nameOnly)
-        return (
-            // Safari DocumentFragment doesn't have getElementById
-            element.getElementById && isSimple && maybeID)
-            ? (
-                (found = element.getElementById(nameOnly))
-                    ? [found]
-                    : []
-            ) : (
-                element.nodeType !== 1 && element.nodeType !== 9 && element.nodeType !== 11
-            )
-                ? []
-                : slice.call(
-                    // DocumentFragment doesn't have getElementsByClassName/TagName
-                    isSimple && !maybeID && element.getElementsByClassName
-                        ? maybeClass
-                            // If it's simple, it could be a class
-                            ? element.getElementsByClassName(nameOnly)
-                            // Or a tag
-                            : element.getElementsByTagName(selector)
-                        // Or it's not simple, and we need to query all
-                        : element.querySelectorAll(selector)
+    D.extend({
+        // Make DOM Array
+        makeArray: function (dom, selector, me) {
+            var i, len = dom ? dom.length : 0
+            for (i = 0; i < len; i++) me[i] = dom[i]
+            me.length = len
+            me.selector = selector || ''
+            return me;
+        },
+        type: type,
+        // The function takes a html string and an optional tag name
+        // to generate DOM nodes from the given html string.
+        // The generated DOM nodes are returned as an array.
+        // This function can be overridden in plugins for example to make
+        // it compatible with browsers that don't support the DOM fully.
+        fragment: function (html, name, properties) {
+            var dom, nodes, container
+
+            // A special case optimization for a single tag
+            if (singleTagRE.test(html)) {
+                dom = D(document.createElement(RegExp.$1))
+            }
+
+            if (!dom) {
+
+                if (html.replace) {
+                    html = html.replace(tagExpanderRE, "<$1></$2>")
+                }
+
+                if (name === undefined) {
+                    name = fragmentRE.test(html) && RegExp.$1
+                }
+
+                if (!(name in containers)) {
+                    name = '*'
+                }
+
+                container = containers[name]
+                container.innerHTML = '' + html
+                dom = D.each(slice.call(container.childNodes), function () {
+                    container.removeChild(this)
+                })
+            }
+
+            if (isPlainObject(properties)) {
+                nodes = D(dom)
+                D.each(properties, function (key, value) {
+                    if (methodAttributes.indexOf(key) > -1) nodes[key](value)
+                    else nodes.attr(key, value)
+                })
+            }
+
+            return dom
+        },
+        // D's CSS selector implementation which
+        // uses `document.querySelectorAll` and optimizes for some special cases, like `#id`.
+        // This method can be overridden in plugins.
+        qsa: function (element, selector) {
+            var found,
+                maybeID = selector[0] == '#',
+                maybeClass = !maybeID && selector[0] == '.',
+                // Ensure that a 1 char tag name still gets checked
+                nameOnly = maybeID || maybeClass ? selector.slice(1) : selector,
+                isSimple = simpleSelectorRE.test(nameOnly)
+            return (
+                // Safari DocumentFragment doesn't have getElementById
+                element.getElementById && isSimple && maybeID)
+                ? (
+                    (found = element.getElementById(nameOnly))
+                        ? [found]
+                        : []
+                ) : (
+                    element.nodeType !== 1 && element.nodeType !== 9 && element.nodeType !== 11
                 )
-    }
-
-    D.type = type;
-
-    D.isD = function (object) {
-        return object instanceof D
-    }
-
-    // plugin compatibility
-    D.uuid = 0
-    D.support = {}
-    D.expr = {}
-    D.noop = function () { }
-
-    D.map = function (elements, callback) {
-        var value, values = [],
-            i, key
-        if (likeArray(elements))
-            for (i = 0; i < elements.length; i++) {
-                value = callback(elements[i], i)
-                if (value != null) values.push(value)
+                    ? []
+                    : slice.call(
+                        // DocumentFragment doesn't have getElementsByClassName/TagName
+                        isSimple && !maybeID && element.getElementsByClassName
+                            ? maybeClass
+                                // If it's simple, it could be a class
+                                ? element.getElementsByClassName(nameOnly)
+                                // Or a tag
+                                : element.getElementsByTagName(selector)
+                            // Or it's not simple, and we need to query all
+                            : element.querySelectorAll(selector)
+                    )
+        },
+        isD: function (object) {
+            return object instanceof D
+        },
+        noop: function () { },
+        map: function (elements, callback) {
+            var value, values = [],
+                i, key
+            if (likeArray(elements))
+                for (i = 0; i < elements.length; i++) {
+                    value = callback(elements[i], i)
+                    if (value != null) values.push(value)
+                }
+            else
+                for (key in elements) {
+                    value = callback(elements[key], key)
+                    if (value != null) values.push(value)
+                }
+            return flatten(values)
+        },
+        each: function (elements, callback) {
+            var i, key
+            if (likeArray(elements)) {
+                for (i = 0; i < elements.length; i++)
+                    if (callback.call(elements[i], i, elements[i]) === false) return elements
+            } else {
+                for (key in elements)
+                    if (callback.call(elements[key], key, elements[key]) === false) return elements
             }
-        else
-            for (key in elements) {
-                value = callback(elements[key], key)
-                if (value != null) values.push(value)
-            }
-        return flatten(values)
-    }
 
-    D.each = function (elements, callback) {
-        var i, key
-        if (likeArray(elements)) {
-            for (i = 0; i < elements.length; i++)
-                if (callback.call(elements[i], i, elements[i]) === false) return elements
-        } else {
-            for (key in elements)
-                if (callback.call(elements[key], key, elements[key]) === false) return elements
-        }
-
-        return elements
-    }
-
-    D.grep = function (elements, callback) {
-        return filter.call(elements, callback)
-    }
+            return elements
+        },
+        grep: function (elements, callback) {
+            return filter.call(elements, callback)
+        },
+    });
 
     // Populate the class2type map
     D.each("Boolean Number String Function Array Date RegExp Object Error".split(" "), function (i, name) {
@@ -332,83 +394,8 @@
             return false
         }
 
-    D.fn = D.prototype = {
-        constuctor: D,
-        length: 0,
-        // Because a collection acts like an array
-        // copy over these useful array functions.
-        forEach: emptyArray.forEach,
-        reduce: emptyArray.reduce,
-        push: emptyArray.push,
-        sort: emptyArray.sort,
-        splice: emptyArray.splice,
-        indexOf: emptyArray.indexOf,
-        // D's counterpart to jQuery's `$.fn.init` and
-        // takes a CSS selector and an optional context (and handles various
-        // special cases).
-        // This method can be overridden in plugins.
-        init: function (selector, context) {
-            var dom;
-            // If nothing given, return an empty D collection
-            if (!selector) {
-                return new D.init(dom, selector)
-            }
-            // Optimize for string selectors
-            else if (typeof selector == 'string') {
-                selector = selector.trim()
-                // If it's a html fragment, create nodes from it
-                // Note: In both Chrome 21 and Firefox 15, DOM error 12
-                // is thrown if the fragment doesn't begin with <
-                if (selector[0] == '<' && fragmentRE.test(selector)) {
-                    dom = D.fragment(selector, RegExp.$1, context);
-                    selector = null;
-                }
-                // If there's a context, create a collection on that context first, and select
-                // nodes from there
-                else if (context !== undefined) {
-                    return D(context).find(selector)
-                }
-                // If it's a CSS selector, use it to select nodes.
-                else {
-                    dom = D.qsa(document, selector)
-                }
-            }
-            // If a function is given, call it when the DOM is ready
-            else if (isFunction(selector)) {
-                return $(document).ready(selector)
-            }
-            // If a D collection is given, just return it
-            else if (D.isD(selector)) {
-                return selector
-            }
-            // 
-            else {
-                // normalize array if an array of nodes is given
-                if (isArray(selector)) {
-                    dom = compact(selector)
-                }
-                // Wrap DOM nodes.
-                else if (isObject(selector)) {
-                    dom = [selector], selector = null
-                }
-                // If it's a html fragment, create nodes from it
-                else if (fragmentRE.test(selector)) {
-                    dom = D.fragment(selector.trim(), RegExp.$1, context),
-                        selector = null
-                }
-                // If there's a context, create a collection on that context first, and select
-                // nodes from there
-                else if (context !== undefined) {
-                    return D(context).find(selector)
-                }
-                // And last but no least, if it's a CSS selector, use it to select nodes.
-                else {
-                    dom = D.qsa(document, selector)
-                }
-            }
-            // create a new D collection from the nodes found
-            return new D.init(dom, selector)
-        },
+    // Methods in Prototype
+    D.fn.extend({
         concat: function () {
             var i, value, args = []
             for (i = 0; i < arguments.length; i++) {
@@ -437,7 +424,20 @@
                     this.parentNode.removeChild(this)
             })
         },
-
+        find: function (selector) {
+            var result, $this = this
+            if (!selector) result = D()
+            else if (typeof selector == 'object')
+                result = D(selector).filter(function () {
+                    var node = this
+                    return emptyArray.some.call($this, function (parent) {
+                        return D.contains(parent, node)
+                    })
+                })
+            else if (this.length == 1) result = D(D.qsa(this[0], selector))
+            else result = this.map(function () { return D.qsa(this, selector) })
+            return result
+        },
         ready: function (callback) {
             // don't use "interactive" on IE <= 10 (it can fired premature)
             if (document.readyState === "complete" ||
@@ -510,12 +510,12 @@
 
             return this.each(function () { this.style.cssText += ';' + css })
         },
-    }
+    });
 
-    function traverseNode(node, fun) {
-        fun(node)
+    function traverseNode(node, fn) {
+        fn(node)
         for (var i = 0, len = node.childNodes.length; i < len; i++)
-            traverseNode(node.childNodes[i], fun)
+            traverseNode(node.childNodes[i], fn)
     }
 
     // Generate the `after`, `prepend`, `before`, `append`,
@@ -593,10 +593,11 @@
             D(html)[operator](this)
             return this
         }
-    })
+    });
 
 
-    D.init.prototype = D.fn;
+    D.fn.init.prototype = D.fn;
 
     window.D = D;
+
 })();
