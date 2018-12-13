61fe390d528df6997b1f1cfcac71b2ccd4f26bdc
l <nzb329@163.com>
2018-12-13 23:04:04 +0800

    add new methods

--- src/core.js
+++ src/core.js
@@ -387,6 +387,20 @@
                         if (node === parent) return true
                     return false
                 });
+        },
+        matches = function (element, selector) {
+            if (!selector || !element || element.nodeType !== 1) return false
+            var matchesSelector = element.matches || element.webkitMatchesSelector ||
+                element.mozMatchesSelector || element.oMatchesSelector ||
+                element.matchesSelector
+            if (matchesSelector) return matchesSelector.call(element, selector)
+            // fall back to performing a selector:
+            var match, parent = element.parentNode,
+                temp = !parent
+            if (temp) (parent = tempParent).appendChild(element)
+            match = ~D.qsa(parent, selector).indexOf(element)
+            temp && tempParent.removeChild(element)
+            return match
         }
     });
 
@@ -405,7 +419,78 @@
             }
             return concat.apply(D.isD(this) ? this.toArray() : this, args)
         },
+        toArray: function () {
+            return this.get()
+        },
 
+        /* Core */
+        each: function (callback) {
+            emptyArray.every.call(this, function (el, idx) {
+                return callback.call(el, idx, el) !== false
+            })
+            return this
+        },
+        get: function (idx) {
+            return idx === undefined ? slice.call(this) : this[idx >= 0 ? idx : idx + this.length]
+        },
+        size: function () {
+            return this.length
+        },
+        index: function (element) {
+            return element ? this.indexOf(D(element)[0]) : this.parent().children().indexOf(this[0])
+        },
+
+        /* Filter */
+        add: function (selector, context) {
+            return D(uniq(this.concat(D(selector, context))))
+        },
+        contents: function () {
+            return this.map(function () { return this.contentDocument || slice.call(this.childNodes) })
+        },
+
+        filter: function (selector) {
+            if (isFunction(selector)) return this.not(this.not(selector))
+            return D(filter.call(this, function (element) {
+                return D.matches(element, selector)
+            }))
+        },
+        is: function (selector) {
+            return typeof selector == 'string' ? this.length > 0 && D.matches(this[0], selector) :
+                selector && this.selector == selector.selector
+        },
+        not: function (selector) {
+            var nodes = []
+            if (isFunction(selector) && selector.call !== undefined)
+                this.each(function (idx) {
+                    if (!selector.call(this, idx)) nodes.push(this)
+                })
+            else {
+                var excludes = typeof selector == 'string' ? this.filter(selector) :
+                    (likeArray(selector) && isFunction(selector.item)) ? slice.call(selector) : D(selector)
+                this.forEach(function (el) {
+                    if (excludes.indexOf(el) < 0) nodes.push(el)
+                })
+            }
+            return D(nodes)
+        },
+        has: function (selector) {
+            return this.filter(function () {
+                return isObject(selector) ?
+                    D.contains(this, selector) :
+                    D(this).find(selector).size()
+            })
+        },
+        eq: function (idx) {
+            return idx === -1 ? this.slice(idx) : this.slice(idx, +idx + 1)
+        },
+        first: function () {
+            var el = this[0]
+            return el && !isObject(el) ? el : $(el)
+        },
+        last: function () {
+            var el = this[this.length - 1]
+            return el && !isObject(el) ? el : $(el)
+        },
         // `map` and `slice` in the jQuery API work differently
         // from their array counterparts
         map: function (fn) {
@@ -419,12 +504,6 @@
             )
         },
 
-        remove: function () {
-            return this.each(function () {
-                if (this.parentNode != null)
-                    this.parentNode.removeChild(this)
-            })
-        },
         find: function (selector) {
             var result, $this = this
             if (!selector) result = D()
@@ -439,40 +518,157 @@
             else result = this.map(function () { return D.qsa(this, selector) })
             return result
         },
-        ready: function (callback) {
-            // don't use "interactive" on IE <= 10 (it can fired premature)
-            if (document.readyState === "complete" ||
-                (document.readyState !== "loading" && !document.documentElement.doScroll))
-                setTimeout(function () { callback(D) }, 0)
-            else {
-                var handler = function () {
-                    document.removeEventListener("DOMContentLoaded", handler, false)
-                    window.removeEventListener("load", handler, false)
-                    callback(D)
-                }
-                document.addEventListener("DOMContentLoaded", handler, false)
-                window.addEventListener("load", handler, false)
-            }
-            return this
-        },
-        get: function (idx) {
-            return idx === undefined ? slice.call(this) : this[idx >= 0 ? idx : idx + this.length]
-        },
-        toArray: function () {
-            return this.get()
-        },
-        size: function () {
-            return this.length
-        },
-        size: function () {
-            return this.length
-        },
-        each: function (callback) {
-            emptyArray.every.call(this, function (el, idx) {
-                return callback.call(el, idx, el) !== false
+        closest: function (selector, context) {
+            var nodes = [],
+                collection = typeof selector == 'object' && D(selector)
+            this.each(function (_, node) {
+                while (node && !(collection ? collection.indexOf(node) >= 0 : D.matches(node, selector)))
+                    node = node !== context && !isDocument(node) && node.parentNode
+                if (node && nodes.indexOf(node) < 0) nodes.push(node)
             })
-            return this
+            return $(nodes)
         },
+        parents: function (selector) {
+            var ancestors = [],
+                nodes = this
+            while (nodes.length > 0)
+                nodes = D.map(nodes, function (node) {
+                    if ((node = node.parentNode) && !isDocument(node) && ancestors.indexOf(node) < 0) {
+                        ancestors.push(node)
+                        return node
+                    }
+                })
+            return filtered(ancestors, selector)
+        },
+        parent: function (selector) {
+            return filtered(uniq(this.pluck('parentNode')), selector)
+        },
+        children: function (selector) {
+            return filtered(this.map(function () { return children(this) }), selector)
+        },
+        siblings: function (selector) {
+            return filtered(this.map(function (i, el) {
+                return filter.call(children(el.parentNode), function (child) { return child !== el })
+            }), selector)
+        },
+        offsetParent: function () {
+            return this.map(function () {
+                var parent = this.offsetParent || document.body
+                while (parent && !rootNodeRE.test(parent.nodeName) && D(parent).css("position") == "static")
+                    parent = parent.offsetParent
+                return parent
+            })
+        },
+
+        // `pluck` is borrowed from Prototype.js
+        pluck: function (property) {
+            return D.map(this, function (el) { return el[property] })
+        },
+
+        /* Attribute */
+        attr: function (name, value) {
+            var result
+            return (typeof name == 'string' && !(1 in arguments)) ?
+                (0 in this && this[0].nodeType == 1 && (result = this[0].getAttribute(name)) != null ? result : undefined) :
+                this.each(function (idx) {
+                    if (this.nodeType !== 1) return
+                    if (isObject(name))
+                        for (key in name) setAttribute(this, key, name[key])
+                    else setAttribute(this, name, funcArg(this, value, idx, this.getAttribute(name)))
+                })
+        },
+        removeAttr: function (name) {
+            return this.each(function () {
+                this.nodeType === 1 && name.split(' ').forEach(function (attribute) {
+                    setAttribute(this, attribute)
+                }, this)
+            })
+        },
+        prop: function (name, value) {
+            name = propMap[name] || name
+            return (typeof name == 'string' && !(1 in arguments)) ?
+                (this[0] && this[0][name]) :
+                this.each(function (idx) {
+                    if (isObject(name))
+                        for (key in name) this[propMap[key] || key] = name[key]
+                    else this[name] = funcArg(this, value, idx, this[name])
+                })
+        },
+        removeProp: function (name) {
+            name = propMap[name] || name
+            return this.each(function () { delete this[name] })
+        },
+        hasClass: function (name) {
+            if (!name) return false
+            return emptyArray.some.call(this, function (el) {
+                return this.test(className(el))
+            }, classRE(name))
+        },
+        addClass: function (name) {
+            if (!name) return this
+            return this.each(function (idx) {
+                if (!('className' in this)) return
+                classList = []
+                var cls = className(this),
+                    newName = funcArg(this, name, idx, cls)
+                newName.split(/\s+/g).forEach(function (klass) {
+                    if (!D(this).hasClass(klass)) classList.push(klass)
+                }, this)
+                classList.length && className(this, cls + (cls ? " " : "") + classList.join(" "))
+            })
+        },
+        removeClass: function (name) {
+            return this.each(function (idx) {
+                if (!('className' in this)) return
+                if (name === undefined) return className(this, '')
+                classList = className(this)
+                funcArg(this, name, idx, classList).split(/\s+/g).forEach(function (klass) {
+                    classList = classList.replace(classRE(klass), " ")
+                })
+                className(this, classList.trim())
+            })
+        },
+        toggleClass: function (name, when) {
+            if (!name) return this
+            return this.each(function (idx) {
+                var $this = D(this),
+                    names = funcArg(this, name, idx, className(this))
+                names.split(/\s+/g).forEach(function (klass) {
+                    (when === undefined ? !$this.hasClass(klass) : when) ?
+                        $this.addClass(klass) : $this.removeClass(klass)
+                })
+            })
+        },
+        html: function (html) {
+            return 0 in arguments ?
+                this.each(function (idx) {
+                    var originHtml = this.innerHTML
+                    D(this).empty().append(funcArg(this, html, idx, originHtml))
+                }) :
+                (0 in this ? this[0].innerHTML : null)
+        },
+        text: function (text) {
+            return 0 in arguments ?
+                this.each(function (idx) {
+                    var newText = funcArg(this, text, idx, this.textContent)
+                    this.textContent = newText == null ? '' : '' + newText
+                }) :
+                (0 in this ? this.pluck('textContent').join("") : null)
+        },
+        val: function (value) {
+            if (0 in arguments) {
+                if (value == null) value = ""
+                return this.each(function (idx) {
+                    this.value = funcArg(this, value, idx, this.value)
+                })
+            } else {
+                return this[0] && (this[0].multiple ?
+                    D(this[0]).find('option').filter(function () { return this.selected }).pluck('value') :
+                    this[0].value)
+            }
+        },
+
+        /* CSS */
         css: function (property, value) {
             if (arguments.length < 2) {
                 var element = this[0]
@@ -510,6 +706,130 @@
             }
 
             return this.each(function () { this.style.cssText += ';' + css })
+        },
+        scrollTop: function (value) {
+            if (!this.length) return
+            var hasScrollTop = 'scrollTop' in this[0]
+            if (value === undefined) return hasScrollTop ? this[0].scrollTop : this[0].pageYOffset
+            return this.each(hasScrollTop ?
+                function () { this.scrollTop = value } :
+                function () { this.scrollTo(this.scrollX, value) })
+        },
+        scrollLeft: function (value) {
+            if (!this.length) return
+            var hasScrollLeft = 'scrollLeft' in this[0]
+            if (value === undefined) return hasScrollLeft ? this[0].scrollLeft : this[0].pageXOffset
+            return this.each(hasScrollLeft ?
+                function () { this.scrollLeft = value } :
+                function () { this.scrollTo(value, this.scrollY) })
+        },
+        position: function () {
+            if (!this.length) return
+
+            var elem = this[0],
+                // Get *real* offsetParent
+                offsetParent = this.offsetParent(),
+                // Get correct offsets
+                offset = this.offset(),
+                parentOffset = rootNodeRE.test(offsetParent[0].nodeName) ? { top: 0, left: 0 } : offsetParent.offset()
+
+            // Subtract element margins
+            // note: when an element has margin: auto the offsetLeft and marginLeft
+            // are the same in Safari causing offset.left to incorrectly be 0
+            offset.top -= parseFloat(D(elem).css('margin-top')) || 0
+            offset.left -= parseFloat(D(elem).css('margin-left')) || 0
+
+            // Add offsetParent borders
+            parentOffset.top += parseFloat(D(offsetParent[0]).css('border-top-width')) || 0
+            parentOffset.left += parseFloat(D(offsetParent[0]).css('border-left-width')) || 0
+
+            // Subtract the two offsets
+            return {
+                top: offset.top - parentOffset.top,
+                left: offset.left - parentOffset.left
+            }
+        },
+
+
+        /* DOM */
+        remove: function () {
+            return this.each(function () {
+                if (this.parentNode != null)
+                    this.parentNode.removeChild(this)
+            })
+        },
+        empty: function () {
+            return this.each(function () { this.innerHTML = '' })
+        },
+        clone: function () {
+            return this.map(function () { return this.cloneNode(true) })
+        },
+        wrap: function (structure) {
+            var func = isFunction(structure)
+            if (this[0] && !func)
+                var dom = D(structure).get(0),
+                    clone = dom.parentNode || this.length > 1
+
+            return this.each(function (index) {
+                D(this).wrapAll(
+                    func ? structure.call(this, index) :
+                        clone ? dom.cloneNode(true) : dom
+                )
+            })
+        },
+        wrapAll: function (structure) {
+            if (this[0]) {
+                D(this[0]).before(structure = D(structure))
+                var children
+                // drill down to the inmost element
+                while ((children = structure.children()).length) structure = children.first()
+                D(structure).append(this)
+            }
+            return this
+        },
+        wrapInner: function (structure) {
+            var func = isFunction(structure)
+            return this.each(function (index) {
+                var self = D(this),
+                    contents = self.contents(),
+                    dom = func ? structure.call(this, index) : structure
+                contents.length ? contents.wrapAll(dom) : self.append(dom)
+            })
+        },
+        unwrap: function () {
+            this.parent().each(function () {
+                D(this).replaceWith(D(this).children())
+            })
+            return this
+        },
+
+        /* Event */
+        ready: function (callback) {
+            // don't use "interactive" on IE <= 10 (it can fired premature)
+            if (document.readyState === "complete" ||
+                (document.readyState !== "loading" && !document.documentElement.doScroll))
+                setTimeout(function () { callback(D) }, 0)
+            else {
+                var handler = function () {
+                    document.removeEventListener("DOMContentLoaded", handler, false)
+                    window.removeEventListener("load", handler, false)
+                    callback(D)
+                }
+                document.addEventListener("DOMContentLoaded", handler, false)
+                window.addEventListener("load", handler, false)
+            }
+            return this
+        },
+
+        show: function () {
+            return this.each(function () {
+                this.style.display == "none" && (this.style.display = '')
+                if (getComputedStyle(this, '').getPropertyValue("display") == "none")
+                    this.style.display = defaultDisplay(this.nodeName)
+            })
+        },
+        hide: function () {
+            return this.css("display", "none")
         },
     });
 
