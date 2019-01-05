1506427532790488a130f50c5bb21eb57e068c38
nzbin <nzb329@163.com>
2019-01-05 17:47:07 +0800

    build: add modular export

---
+++ src/domq.modular.js
@@ -0,0 +1,17 @@
+import D from './d-class';
+
+export * from './core';
+export * from './css';
+export * from './classes';
+export * from './offset';
+export * from './attr';
+export * from './prop';
+export * from './val';
+export * from './wrap';
+export * from './traversing';
+export * from './dimensions';
+export * from './manipulation';
+export * from './event';
+export * from './animate';
+export * from './effects';
+export { D };
