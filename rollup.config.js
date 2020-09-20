44c5a6850ad59abcf2256f8f8d485b5e953d6b3b
nzbin <nzb329@163.com>
2020-09-20 23:17:20 +0800

    build: correct rollup plugin path

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
 
 const banner = `
